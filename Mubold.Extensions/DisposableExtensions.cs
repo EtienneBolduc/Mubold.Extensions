e46a13b4b3382325a3d3e66c373566f17410beb5
EtienneBolduc <atnbolduc@gmail.com>
2024-06-28 08:04:07 -0400

    Test

---
+++ Mubold.Extensions/DisposableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Reactive.Disposables;
+
+namespace Mubold.Extensions;
+
+public static class DisposableMixins
+{
+	public static T DisposeWith<T>(this T item, CompositeDisposable compositeDisposable)
+		where T : IDisposable
+	{
+		if (compositeDisposable is null)
+		{
+			throw new ArgumentNullException(nameof(compositeDisposable));
+		}
+
+		compositeDisposable.Add(item);
+		return item;
+	}
+}
